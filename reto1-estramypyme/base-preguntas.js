7fa1425f8ed5838d8660bcb92bc8754ff4b4c14f
leydimadrid <leydi.madrid6@gmail.com>
2024-06-28 15:05:40 -0500

    Se muestran todas las preguntas de las 3 secciones

--- reto1-estramypyme/base-preguntas.js
+++ reto1-estramypyme/base-preguntas.js
@@ -1,4 +1,5 @@
 let basePreguntas = [
+  // // Sección "¿Por qué?"
   {
     id: 1,
     pregunta:
@@ -47,6 +48,112 @@ let basePreguntas = [
       opcion1: "Nada influyente",
       opcion2: "Poco influyente",
       opcion3: "Muy influyente",
+    },
+  },
+
+  
+  // Sección "¿Cómo?"
+  {
+    id: 6,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan claro están los valores y principios de la empresa para todos los empleados?",
+    opciones: {
+      opcion1: "Nada claro",
+      opcion2: "Poco claro",
+      opcion3: "Muy claro",
+    },
+  },
+  {
+    id: 7,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan bien se implementan los valores y principios en las operaciones diarias?",
+    opciones: {
+      opcion1: "Muy mal",
+      opcion2: "Mas o menos",
+      opcion3: "Muy bien",
+    },
+  },
+  {
+    id: 8,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan diferenciadores son sus métodos y prácticas frente a la competencia?",
+    opciones: {
+      opcion1: "Nada diferenciadores",
+      opcion2: "Poco diferenciadores",
+      opcion3: "Muy diferenciadores",
+    },
+  },
+  {
+    id: 9,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan consistentes son los procesos de la empresa con sus valores y principios?",
+    opciones: {
+      opcion1: "Nada consistentes",
+      opcion2: "Poco consistentes",
+      opcion3: "Muy consistentes",
+    },
+  },
+
+
+  // Sección "¿Qué?"
+  {
+    id: 10,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan innovadores son los métodos y procesos utilizados por la empresa?",
+    opciones: {
+      opcion1: "Nada innovadores",
+      opcion2: "Poco innovadores",
+      opcion3: "Muy innovadores",
+    },
+  },
+  {
+    id: 11,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan alta es la calidad de los productos/servicios ofrecidos por la empresa?",
+    opciones: {
+      opcion1: "Muy baja",
+      opcion2: "Mas o menos",
+      opcion3: "Muy alta",
+    },
+  },
+  {
+    id: 12,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan bien se alinean los productos/servicios con el propósito de la empresa?",
+    opciones: {
+      opcion1: "Nada bien",
+      opcion2: "Mas o menos",
+      opcion3: "Muy bien",
+    },
+  },
+  {
+    id: 13,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan satisfechos están los clientes con los productos/servicios de la empresa?",
+    opciones: {
+      opcion1: "Nada satisfechos",
+      opcion2: "Poco satisfechos ",
+      opcion3: "Muy satisfechos",
+    },
+  },
+  {
+    id: 14,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan innovadores son los productos/servicios de la empresa?",
+    opciones: {
+      opcion1: "Nada innovadores",
+      opcion2: "Poco innovadores",
+      opcion3: "Muy innovadores",
+    },
+  },
+  {
+    id: 15,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan amplia es la variedad de productos/servicios que ofrece la empresa?",
+    opciones: {
+      opcion1: "Nada amplia",
+      opcion2: "Poco amplia",
+      opcion3: "Muy amplia",
     },
   },
 ];
