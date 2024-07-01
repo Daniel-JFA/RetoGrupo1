7d9bde02177688f2e90c83a63485ee6539e32c4a
ReinaldoRojas <rojas.reinaldo@hotmail.com>
2024-07-01 14:04:31 -0500

    Creación de grafico círculo dorado con modificaciones en los estilos, html y en script.js

---
+++ reto1-estramypyme/base-preguntas.js
@@ -0,0 +1,157 @@
+let basePreguntas = [
+  // // Sección "¿Por qué?"
+  {
+    id: 0,
+    pregunta:
+      "En una escala del 1 al 3, ¿qué tan claro está el propósito fundamental de su empresa para todos los empleados?",
+    opciones: {
+      opcion1: "Nada claro",
+      opcion2: "Algo claro",
+      opcion3: "Muy claro",
+    },
+  },
+  {
+    id: 1,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan comprometidos se sienten los empleados con la misión de la empresa?",
+    opciones: {
+      opcion1: "Nada comprometidos",
+      opcion2: "Poco comprometidos",
+      opcion3: "Muy comprometidos",
+    },
+  },
+  {
+    id: 2,
+    pregunta:
+      "En una escala del 1 al 3, ¿Con qué frecuencia se comunica el propósito de la empresa a los empleados?",
+    opciones: {
+      opcion1: "Nunca",
+      opcion2: "Poco frecuente",
+      opcion3: "Muy frecuente",
+    },
+  },
+  {
+    id: 3,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan alineadas están las decisiones estratégicas de la empresa con su propósito fundamental?",
+    opciones: {
+      opcion1: "Nada alineadas",
+      opcion2: "Poco alineadas",
+      opcion3: "Muy alineadas",
+    },
+  },
+  {
+    id: 4,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan influyente es el propósito de la empresa en la cultura organizacional?",
+    opciones: {
+      opcion1: "Nada influyente",
+      opcion2: "Poco influyente",
+      opcion3: "Muy influyente",
+    },
+  },
+
+  // Sección "¿Cómo?"
+  {
+    id: 5,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan claro están los valores y principios de la empresa para todos los empleados?",
+    opciones: {
+      opcion1: "Nada claro",
+      opcion2: "Poco claro",
+      opcion3: "Muy claro",
+    },
+  },
+  {
+    id: 6,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan bien se implementan los valores y principios en las operaciones diarias?",
+    opciones: {
+      opcion1: "Muy mal",
+      opcion2: "Mas o menos",
+      opcion3: "Muy bien",
+    },
+  },
+  {
+    id: 7,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan diferenciadores son sus métodos y prácticas frente a la competencia?",
+    opciones: {
+      opcion1: "Nada diferenciadores",
+      opcion2: "Poco diferenciadores",
+      opcion3: "Muy diferenciadores",
+    },
+  },
+  {
+    id: 8,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan consistentes son los procesos de la empresa con sus valores y principios?",
+    opciones: {
+      opcion1: "Nada consistentes",
+      opcion2: "Poco consistentes",
+      opcion3: "Muy consistentes",
+    },
+  },
+
+  // Sección "¿Qué?"
+  {
+    id: 9,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan innovadores son los métodos y procesos utilizados por la empresa?",
+    opciones: {
+      opcion1: "Nada innovadores",
+      opcion2: "Poco innovadores",
+      opcion3: "Muy innovadores",
+    },
+  },
+  {
+    id: 10,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan alta es la calidad de los productos/servicios ofrecidos por la empresa?",
+    opciones: {
+      opcion1: "Muy baja",
+      opcion2: "Mas o menos",
+      opcion3: "Muy alta",
+    },
+  },
+  {
+    id: 11,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan bien se alinean los productos/servicios con el propósito de la empresa?",
+    opciones: {
+      opcion1: "Nada bien",
+      opcion2: "Mas o menos",
+      opcion3: "Muy bien",
+    },
+  },
+  {
+    id: 12,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan satisfechos están los clientes con los productos/servicios de la empresa?",
+    opciones: {
+      opcion1: "Nada satisfechos",
+      opcion2: "Poco satisfechos ",
+      opcion3: "Muy satisfechos",
+    },
+  },
+  {
+    id: 13,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan innovadores son los productos/servicios de la empresa?",
+    opciones: {
+      opcion1: "Nada innovadores",
+      opcion2: "Poco innovadores",
+      opcion3: "Muy innovadores",
+    },
+  },
+  {
+    id: 14,
+    pregunta:
+      "En una escala del 1 al 3, ¿Qué tan amplia es la variedad de productos/servicios que ofrece la empresa?",
+    opciones: {
+      opcion1: "Nada amplia",
+      opcion2: "Poco amplia",
+      opcion3: "Muy amplia",
+    },
+  },
+];
---
+++ reto1-estramypyme/script.js
@@ -0,0 +1,249 @@
+var chart = echarts.init(document.getElementById('main'));
+var indexPregunta = 0;
+// Variable que almacena el valor del progreso y es reciclada en la función progreso
+var valorProgreso = 0;
+//invocación de la función de progreso que crea el gráfico para que se muestre al cargar la página
+progreso(indexPregunta)
+//invocación de la función que crea el gráfico de circulo dorado para que se muestre al cargar la página
+graficoRespuestas(indexPregunta)
+
+function cargarPregunta(id) {
+  // document
+  //   .querySelectorAll(".contenedor-respuestas input[type='radio']")
+  //   .forEach((radio) => {
+  //     radio.checked = false;
+  //   });
+
+  objetoPregunta = basePreguntas[id];
+  opciones = objetoPregunta.opciones;
+  document.getElementById("progreso").innerHTML = `Pregunta ${indexPregunta + 1
+    } de ${basePreguntas.length}`;
+  document.getElementById("contenedor-parrafo").innerHTML =
+    objetoPregunta.pregunta;
+
+  document.querySelector("label#opcion1").innerHTML = opciones.opcion1;
+  document.querySelector("label#opcion2").innerHTML = opciones.opcion2;
+  document.querySelector("label#opcion3").innerHTML = opciones.opcion3;
+}
+
+function seleccionarOpcion(id) {
+  let validezRespuesta =
+    (opciones[id] == objetoPregunta.opcion1) | opcion2 | opcion3;
+  if (validezRespuesta) {
+    indexPregunta++;
+    //llamada a la función de progreso que crea el gráfico
+    progreso(indexPregunta)
+    //llamada a la función que crea el gráfico de respuestas
+    graficoRespuestas(indexPregunta)
+    
+    if (indexPregunta == 5) {
+      Swal.fire({
+        title: "¡Bien hecho, has terminado la sección '¿Por qué?'! 👏",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 5;
+        document.getElementById("como").checked = true;
+        cargarPregunta(indexPregunta);
+      });
+    } else if (indexPregunta == 10) {
+      Swal.fire({
+        title: "¡Bien hecho, has terminado la sección '¿Cómo?!' 👏",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 10;
+        document.getElementById("que").checked = true;
+        cargarPregunta(indexPregunta);
+      });
+    } else if (indexPregunta == 15) {
+      Swal.fire({
+        title: "¡Bien hecho, has finalizado todas las secciones' 👏",
+        text: "¡Modelo Círculo Dorado completado!🎉",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 0;
+        document.getElementById("porque").checked = true;
+        cargarPregunta(indexPregunta);
+      });
+    }
+  }
+  cargarPregunta(indexPregunta);
+}
+//esta fución se encarga de crear el gráfico de progreso
+function progreso(indexPregunta) {
+  valorProgreso = indexPregunta * 7;
+  if (valorProgreso > 100) {
+    valorProgreso = 100;
+  }
+
+  var option = {
+    series: [
+      {
+        type: 'gauge',
+        startAngle: 180,
+        endAngle: 0,
+        center: ['50%', '50%'],
+        radius: '100%',
+        pointer: {
+          show: true
+        },
+        progress: {
+          show: true,
+          overlap: false,
+          roundCap: true,
+          clip: false,
+          itemStyle: {
+            borderWidth: 1,
+            borderColor: '#464646'
+          }
+        },
+        axisLine: {
+          lineStyle: {
+            width: 30
+          }
+        },
+        splitLine: {
+          show: true,
+          length: 30,
+          lineStyle: {
+            color: 'auto',
+            width: 2
+          }
+        },
+        axisTick: {
+          show: true,
+          splitNumber: 1, // Número de pequeños ticks entre los principales
+          length: 15,
+          lineStyle: {
+            color: 'auto',
+            width: 2
+          }
+        },
+        axisLabel: {
+          show: true,
+          distance: 30,
+          fontSize: 15,
+          formatter: function (value) {
+            if (value % (100 / 15) === 0) {
+              return value;
+            } else {
+              return '';
+            }
+          }
+        },
+        data: [
+          {
+            value: valorProgreso,
+            name: 'Progreso',
+            title: {
+              offsetCenter: ['0%', '-30%']
+            },
+            detail: {
+              offsetCenter: ['0%', '45%']
+            }
+          }
+        ],
+        title: {
+          fontSize: 14
+        },
+        detail: {
+          width: 50,
+          height: 14,
+          fontSize: 20,
+          color: 'auto',
+          formatter: '{value}%'
+        }
+      }
+    ]
+  };
+
+  chart.setOption(option);
+}
+
+//esta función se encarga de crear el gráfico de circulo dorado
+function graficoRespuestas(indexPregunta) {
+  var chartDom = document.getElementById('mainRespuestas');
+  var myChart = echarts.init(chartDom);
+  let porQue = 5;
+  let como = 5;
+  let que = 5;
+
+  if(indexPregunta <= 5){
+    porQue = indexPregunta;
+    como = 0;
+    que = 0;
+  }
+  else if(indexPregunta > 5 && indexPregunta <= 10){
+    como = indexPregunta - 5;
+    porQue = 5;
+    que = 0;
+  }
+  else if(indexPregunta <= 15){
+    que = indexPregunta - 10;
+    porQue = 5;
+    como = 5;
+    //este codigo sobra pero lo dejo para luesgo ver como solucionar el problema de la variable 'que'  que no se actualiza
+  }else if(indexPregunta == 15){
+    porQue = 5;
+    como = 5;
+    que = 5;
+  }
+
+
+  var option;
+
+  option = {
+    title: [
+      {
+        text: '', // aca va el titulo lo quite porque no se veia bien
+        left: 'center',
+        top: 0,
+
+      }
+    ],
+    polar: {
+      radius: [2, '100%']
+    },
+    angleAxis: {
+      max: 5,
+      startAngle: 90,
+      show: false // Ocultar las etiquetas y las líneas del eje angular
+    },
+    radiusAxis: {
+      type: 'category',
+      data: ['¿POR QUE?', '¿COMO?', '¿QUÉ?'],
+      show: false // Ocultar las etiquetas y las líneas del eje radial
+    },
+    tooltip: {},
+    series: {
+      type: 'bar',
+      data: [
+        { value: porQue, itemStyle: { color: '#11d18e' } },
+        { value: como, itemStyle: { color: '#d1113e' } },
+        { value: que, itemStyle: { color: '#741278' } },
+      ],
+      coordinateSystem: 'polar',
+      barWidth: 110, // Ancho de las barras
+      barGap: '0%', // Espacio entre las barras
+      label: {
+        show: true,
+        position: 'middle',
+        formatter: '{b}',
+        color: '#fff', // Color de la etiqueta
+        fontSize: 18 // Tamaño de la fuente de la etiqueta
+      }
+    }
+  };
+  
+  option && myChart.setOption(option);
+
+  window.addEventListener('resize', function () {
+    myChart.resize();
+  });
+}
+
+
+cargarPregunta(indexPregunta);
+graficoRespuestas()
