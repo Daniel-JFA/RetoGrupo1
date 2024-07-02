262e6a7fc9af4e5ab1db5ba1440a0cf0d1d80cb5
ReinaldoRojas <rojas.reinaldo@hotmail.com>
2024-07-02 07:53:10 -0500

    Ajustes codigo

--- reto1-estramypyme/script.js
+++ reto1-estramypyme/script.js
@@ -1,11 +1,11 @@
-var chart = echarts.init(document.getElementById("main"));
+var chart = echarts.init(document.getElementById('main'));
 var indexPregunta = 0;
 // Variable que almacena el valor del progreso y es reciclada en la función progreso
 var valorProgreso = 0;
 //invocación de la función de progreso que crea el gráfico para que se muestre al cargar la página
-progreso(indexPregunta);
+progreso(indexPregunta)
 //invocación de la función que crea el gráfico de circulo dorado para que se muestre al cargar la página
-graficoRespuestas(indexPregunta);
+graficoRespuestas(indexPregunta)
 
 function cargarPregunta(id) {
   // document
@@ -16,11 +16,8 @@ function cargarPregunta(id) {
 
   objetoPregunta = basePreguntas[id];
   opciones = objetoPregunta.opciones;
-
-  document.getElementById("progreso").innerHTML = `Pregunta ${
-    indexPregunta + 1
-  } de ${basePreguntas.length}`;
-
+  document.getElementById("progreso").innerHTML = `Pregunta ${indexPregunta + 1
+    } de ${basePreguntas.length}`;
   document.getElementById("contenedor-parrafo").innerHTML =
     objetoPregunta.pregunta;
 
@@ -32,53 +29,48 @@ function cargarPregunta(id) {
 function seleccionarOpcion(id) {
   let validezRespuesta =
     (opciones[id] == objetoPregunta.opcion1) | opcion2 | opcion3;
-
   if (validezRespuesta) {
     indexPregunta++;
     //llamada a la función de progreso que crea el gráfico
-    progreso(indexPregunta);
+    progreso(indexPregunta)
     //llamada a la función que crea el gráfico de respuestas
-    graficoRespuestas(indexPregunta);
-
-    if (validezRespuesta) {
-      indexPregunta++;
-      if (indexPregunta == 5) {
-        Swal.fire({
-          title: "¡Bien hecho, has terminado la sección '¿Por qué?'! 👏",
-          customClass: "my-custom-class",
-        }).then(() => {
-          // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
-          indexPregunta = 5;
-          document.getElementById("como").checked = true;
-          cargarPregunta(indexPregunta);
-        });
-      } else if (indexPregunta == 10) {
-        Swal.fire({
-          title: "¡Bien hecho, has terminado la sección '¿Cómo?!' 👏",
-          customClass: "my-custom-class",
-        }).then(() => {
-          // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
-          indexPregunta = 10;
-          document.getElementById("que").checked = true;
-          cargarPregunta(indexPregunta);
-        });
-      } else if (indexPregunta == 15) {
-        Swal.fire({
-          title: "¡Bien hecho, has finalizado todas las secciones' 👏",
-          text: "¡Modelo Círculo Dorado completado!🎉",
-          customClass: "my-custom-class",
-        }).then(() => {
-          // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
-          indexPregunta = 0;
-          document.getElementById("porque").checked = true;
-          cargarPregunta(indexPregunta);
-        });
-      }
+    graficoRespuestas(indexPregunta)
+    
+    if (indexPregunta == 5) {
+      Swal.fire({
+        title: "¡Bien hecho, has terminado la sección '¿Por qué?'! 👏",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 5;
+        document.getElementById("como").checked = true;
+        cargarPregunta(indexPregunta);
+      });
+    } else if (indexPregunta == 10) {
+      Swal.fire({
+        title: "¡Bien hecho, has terminado la sección '¿Cómo?!' 👏",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 10;
+        document.getElementById("que").checked = true;
+        cargarPregunta(indexPregunta);
+      });
+    } else if (indexPregunta == 15) {
+      Swal.fire({
+        title: "¡Bien hecho, has finalizado todas las secciones' 👏",
+        text: "¡Modelo Círculo Dorado completado!🎉",
+        customClass: "my-custom-class",
+      }).then(() => {
+        // Cuando el modal se cierre, activa la pestaña ¿Cómo? y carga la pregunta 6
+        indexPregunta = 0;
+        document.getElementById("porque").checked = true;
+        cargarPregunta(indexPregunta);
+      });
     }
-    cargarPregunta(indexPregunta);
   }
+  cargarPregunta(indexPregunta);
 }
-
 //esta fución se encarga de crear el gráfico de progreso
 function progreso(indexPregunta) {
   valorProgreso = indexPregunta * 7;
@@ -89,13 +81,13 @@ function progreso(indexPregunta) {
   var option = {
     series: [
       {
-        type: "gauge",
+        type: 'gauge',
         startAngle: 180,
         endAngle: 0,
-        center: ["50%", "50%"],
-        radius: "100%",
+        center: ['50%', '50%'],
+        radius: '100%',
         pointer: {
-          show: true,
+          show: true
         },
         progress: {
           show: true,
@@ -104,30 +96,30 @@ function progreso(indexPregunta) {
           clip: false,
           itemStyle: {
             borderWidth: 1,
-            borderColor: "#464646",
-          },
+            borderColor: '#464646'
+          }
         },
         axisLine: {
           lineStyle: {
-            width: 30,
-          },
+            width: 30
+          }
         },
         splitLine: {
           show: true,
           length: 30,
           lineStyle: {
-            color: "auto",
-            width: 2,
-          },
+            color: 'auto',
+            width: 2
+          }
         },
         axisTick: {
           show: true,
           splitNumber: 1, // Número de pequeños ticks entre los principales
           length: 15,
           lineStyle: {
-            color: "auto",
-            width: 2,
-          },
+            color: 'auto',
+            width: 2
+          }
         },
         axisLabel: {
           show: true,
@@ -137,34 +129,34 @@ function progreso(indexPregunta) {
             if (value % (100 / 15) === 0) {
               return value;
             } else {
-              return "";
+              return '';
             }
-          },
+          }
         },
         data: [
           {
             value: valorProgreso,
-            name: "Progreso",
+            name: 'Progreso',
             title: {
-              offsetCenter: ["0%", "-30%"],
+              offsetCenter: ['0%', '-30%']
             },
             detail: {
-              offsetCenter: ["0%", "45%"],
-            },
-          },
+              offsetCenter: ['0%', '45%']
+            }
+          }
         ],
         title: {
-          fontSize: 14,
+          fontSize: 14
         },
         detail: {
           width: 50,
           height: 14,
           fontSize: 20,
-          color: "auto",
-          formatter: "{value}%",
-        },
-      },
-    ],
+          color: 'auto',
+          formatter: '{value}%'
+        }
+      }
+    ]
   };
 
   chart.setOption(option);
@@ -172,83 +164,86 @@ function progreso(indexPregunta) {
 
 //esta función se encarga de crear el gráfico de circulo dorado
 function graficoRespuestas(indexPregunta) {
-  var chartDom = document.getElementById("mainRespuestas");
+  var chartDom = document.getElementById('mainRespuestas');
   var myChart = echarts.init(chartDom);
   let porQue = 5;
   let como = 5;
   let que = 5;
 
-  if (indexPregunta <= 5) {
+  if(indexPregunta <= 5){
     porQue = indexPregunta;
     como = 0;
     que = 0;
-  } else if (indexPregunta > 5 && indexPregunta <= 10) {
+  }
+  else if(indexPregunta > 5 && indexPregunta <= 10){
     como = indexPregunta - 5;
     porQue = 5;
     que = 0;
-  } else if (indexPregunta <= 15) {
+  }
+  else if(indexPregunta <= 15){
     que = indexPregunta - 10;
     porQue = 5;
     como = 5;
     //este codigo sobra pero lo dejo para luesgo ver como solucionar el problema de la variable 'que'  que no se actualiza
-  } else if (indexPregunta == 15) {
+  }else if(indexPregunta == 15){
     porQue = 5;
     como = 5;
     que = 5;
   }
+
 
   var option;
 
   option = {
     title: [
       {
-        text: "", // aca va el titulo lo quite porque no se veia bien
-        left: "center",
+        text: '', // aca va el titulo lo quite porque no se veia bien
+        left: 'center',
         top: 0,
-      },
+
+      }
     ],
     polar: {
-      radius: [2, "100%"],
+      radius: [2, '100%']
     },
     angleAxis: {
       max: 5,
       startAngle: 90,
-      show: false, // Ocultar las etiquetas y las líneas del eje angular
+      show: false // Ocultar las etiquetas y las líneas del eje angular
     },
     radiusAxis: {
-      type: "category",
-      data: ["¿POR QUE?", "¿COMO?", "¿QUÉ?"],
-      show: false, // Ocultar las etiquetas y las líneas del eje radial
+      type: 'category',
+      data: ['¿POR QUE?', '¿COMO?', '¿QUÉ?'],
+      show: false // Ocultar las etiquetas y las líneas del eje radial
     },
     tooltip: {},
     series: {
-      type: "bar",
+      type: 'bar',
       data: [
-        { value: porQue, itemStyle: { color: "#11d18e" } },
-        { value: como, itemStyle: { color: "#d1113e" } },
-        { value: que, itemStyle: { color: "#741278" } },
+        { value: porQue, itemStyle: { color: '#11d18e' } },
+        { value: como, itemStyle: { color: '#d1113e' } },
+        { value: que, itemStyle: { color: '#741278' } },
       ],
-      coordinateSystem: "polar",
+      coordinateSystem: 'polar',
       barWidth: 110, // Ancho de las barras
-      barGap: "0%", // Espacio entre las barras
+      barGap: '0%', // Espacio entre las barras
       label: {
         show: true,
-        position: "middle",
-        formatter: "{b}",
-        color: "#fff", // Color de la etiqueta
-        fontSize: 18, // Tamaño de la fuente de la etiqueta
-      },
-    },
+        position: 'middle',
+        formatter: '{b}',
+        color: '#fff', // Color de la etiqueta
+        fontSize: 18 // Tamaño de la fuente de la etiqueta
+      }
+    }
   };
-
+  
   option && myChart.setOption(option);
 
-  window.addEventListener("resize", function () {
+  window.addEventListener('resize', function () {
     myChart.resize();
   });
 }
 
-cargarPregunta(indexPregunta);
-graficoRespuestas();
 
 cargarPregunta(indexPregunta);
+graficoRespuestas()
