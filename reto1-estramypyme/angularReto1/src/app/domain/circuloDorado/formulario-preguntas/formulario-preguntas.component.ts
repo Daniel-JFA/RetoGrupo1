b70fd94d8f9523aea6b77c6cff28c268ea3d6648
leydimadrid <leydi.madrid6@gmail.com>
2024-07-07 18:40:59 -0500

    Se agrega paginador, falta terminar css y lógica

--- reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/formulario-preguntas/formulario-preguntas.component.ts
+++ reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/formulario-preguntas/formulario-preguntas.component.ts
@@ -3,7 +3,9 @@ import {
   Component,
   ElementRef,
   OnInit,
+  SimpleChange,
   ViewChild,
+  ViewEncapsulation,
 } from '@angular/core';
 import { PreguntasService } from '../preguntas.service';
 import { FormsModule } from '@angular/forms';
@@ -16,6 +18,7 @@ import Swal from 'sweetalert2';
   imports: [FormsModule],
   templateUrl: './formulario-preguntas.component.html',
   styleUrl: './formulario-preguntas.component.css',
+
 })
 export class FormularioPreguntasComponent implements OnInit {
   preguntas: any[] = [];
@@ -24,22 +27,27 @@ export class FormularioPreguntasComponent implements OnInit {
   objetoPregunta: any;
   opciones: any;
   seleccionada: boolean = true;
-  chart: any;
   isPorQueChecked: boolean = true;
   isComoChecked: boolean = false;
   isQueChecked: boolean = false;
+  isActive: boolean = true;
 
   constructor(private preguntaService: PreguntasService) {}
 
   ngOnInit(): void {
     this.preguntas = this.preguntaService.getPreguntas();
-    this.cargarPregunta(this.indexPregunta);
+    console.log(this.cargarPregunta(this.indexPregunta));
   }
 
   cargarPregunta(index: number) {
-    this.seleccionada = false;
-    this.objetoPregunta = this.preguntas[index];
-    this.opciones = this.objetoPregunta.opciones;
+    if (index < this.preguntas.length) {
+      this.seleccionada = false;
+      this.objetoPregunta = this.preguntas[index];
+      this.opciones = this.objetoPregunta.opciones;
+      console.log(`Cargando pregunta con índice: ${index}`);
+    } else {
+      console.log('No hay más preguntas.');
+    }
   }
 
   manejarSiguiente() {
@@ -51,15 +59,15 @@ export class FormularioPreguntasComponent implements OnInit {
       return;
     }
 
-    // Incrementa el índice de la pregunta para avanzar a la siguiente.
     this.indexPregunta++;
+
+    // Incrementa el índice de la pregunta para avanzar a la siguiente.
 
     if (this.indexPregunta == 5) {
       Swal.fire({
         title: "¡Bien hecho, has terminado la sección '¿Por qué?'! 👏",
         // customClass: 'my-custom-class',
       }).then(() => {
-        // indexPregunta = 5; // Eliminado
         this.isComoChecked = true;
         this.cargarPregunta(this.indexPregunta);
       });
@@ -68,11 +76,10 @@ export class FormularioPreguntasComponent implements OnInit {
         title: "¡Bien hecho, has terminado la sección '¿Cómo?!' 👏",
         // customClass: 'my-custom-class',
       }).then(() => {
-        // indexPregunta = 10; // Eliminado
         this.isQueChecked = true;
         this.cargarPregunta(this.indexPregunta);
       });
-    } else if (this.indexPregunta >= 15) {
+    } else if (this.indexPregunta == 15) {
       Swal.fire({
         title: "¡Bien hecho, has finalizado todas las secciones' 👏",
         text: '¡Modelo Círculo Dorado completado!🎉',
@@ -84,7 +91,7 @@ export class FormularioPreguntasComponent implements OnInit {
       });
     }
     this.cargarPregunta(this.indexPregunta); // Carga la nueva pregunta.
-    this.progreso(this.indexPregunta); // Actualiza el progreso.
+    // this.progreso(this.indexPregunta); // Actualiza el progreso.
     // this.graficoRespuestas(this.indexPregunta); // Actualiza el gráfico de respuestas.
   }
 
@@ -92,101 +99,7 @@ export class FormularioPreguntasComponent implements OnInit {
     this.indexPregunta--; // Disminuye el índice de la pregunta para retroceder a la anterior.
 
     this.cargarPregunta(this.indexPregunta); // Carga la nueva pregunta.
-    this.progreso(this.indexPregunta); // Actualiza el progreso.
+    // this.progreso(this.indexPregunta); // Actualiza el progreso.
     // this.graficoRespuestas(this.indexPregunta); // Actualiza el gráfico de respuestas.  }
-  }
-
-  progreso(_index: number) {
-    
-    this.valorProgreso = this.indexPregunta * 7;
-    if (this.valorProgreso > 100) {
-      this.valorProgreso = 100;
-    }
-
-    var option = {
-      series: [
-        {
-          type: 'gauge',
-          startAngle: 180,
-          endAngle: 0,
-          center: ['50%', '50%'],
-          radius: '100%',
-          pointer: {
-            show: true,
-          },
-          progress: {
-            show: true,
-            overlap: false,
-            roundCap: true,
-            clip: false,
-            itemStyle: {
-              borderWidth: 1,
-              borderColor: '#464646',
-            },
-          },
-          axisLine: {
-            lineStyle: {
-              width: 30,
-            },
-          },
-          splitLine: {
-            show: true,
-            length: 30,
-            lineStyle: {
-              color: 'auto',
-              width: 2,
-            },
-          },
-          axisTick: {
-            show: true,
-            splitNumber: 1, // Número de pequeños ticks entre los principales
-            length: 15,
-            lineStyle: {
-              color: 'auto',
-              width: 2,
-            },
-          },
-          axisLabel: {
-            show: true,
-            distance: 30,
-            fontSize: 15,
-            formatter: function (value: number) {
-              if (value % (100 / 15) === 0) {
-                return value;
-              } else {
-                return '';
-              }
-            },
-          },
-          data: [
-            {
-              value: this.valorProgreso,
-              name: 'Progreso',
-              title: {
-                offsetCenter: ['0%', '-30%'],
-              },
-              detail: {
-                offsetCenter: ['0%', '45%'],
-              },
-            },
-          ],
-          title: {
-            fontSize: 14,
-          },
-          detail: {
-            width: 50,
-            height: 14,
-            fontSize: 20,
-            color: 'auto',
-            formatter: '{value}%',
-          },
-        },
-      ],
-    };
-
-    // window.addEventListener("resize", function () {
-    //   myChart.resize();
-    // });
-    this.chart.setOption(option);
   }
 }
