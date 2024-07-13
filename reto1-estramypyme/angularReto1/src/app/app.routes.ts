12effdb1eb3863452ac2f5b9ce39cdd83fd7a675
ReinaldoRojas <rojas.reinaldo@hotmail.com>
2024-07-12 19:39:11 -0500

    Creación de componente de resultados y enrutamiento en header

--- reto1-estramypyme/angularReto1/src/app/app.routes.ts
+++ reto1-estramypyme/angularReto1/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CirculoDoradoPageComponent } from './domain/circuloDorado/circulo-dorado-page/circulo-dorado-page.component';
 import { RadarEstrategicoPageComponent } from './domain/radarEstrategico/radar-estrategico-page/radar-estrategico-page.component';
+import { ResultadosComponent } from './domain/resultados/resultados.component';
 
 export const routes: Routes = [
   { path: '', component: CirculoDoradoPageComponent },
   { path: 'circulo-dorado', component: CirculoDoradoPageComponent },
   { path: 'radar-estrategico', component: RadarEstrategicoPageComponent },
+  { path: 'resultados', component: ResultadosComponent },
 ];
