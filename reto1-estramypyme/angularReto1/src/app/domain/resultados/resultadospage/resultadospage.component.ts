5a05931af474cb2fa32a82236b0b60284521eae7
leydimadrid <leydi.madrid6@gmail.com>
2024-07-17 19:42:46 -0500

    cambios en el responsive

--- reto1-estramypyme/angularReto1/src/app/domain/resultados/resultadospage/resultadospage.component.ts
+++ reto1-estramypyme/angularReto1/src/app/domain/resultados/resultadospage/resultadospage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ResultadosRadarComponent } from '../resultados-radar/resultados-radar.component';
 import { ResultadosCirculoComponent } from '../resultados-circulo/resultados-circulo.component';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-resultadospage',
   standalone: true,
-  imports: [ResultadosRadarComponent, ResultadosCirculoComponent],
+  imports: [ResultadosRadarComponent, ResultadosCirculoComponent, HeaderComponent],
   templateUrl: './resultadospage.component.html',
   styleUrl: './resultadospage.component.css'
 })
