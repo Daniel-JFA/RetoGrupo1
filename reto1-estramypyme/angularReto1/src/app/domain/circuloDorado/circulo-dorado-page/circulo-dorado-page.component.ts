d458e22ff5e86b54e018c358eafcdab64a98a775
leydimadrid <leydi.madrid6@gmail.com>
2024-07-25 15:00:09 -0500

    Se agrega al localStorage el estado del formulario y progresos de gráfica

--- reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/circulo-dorado-page/circulo-dorado-page.component.ts
+++ reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/circulo-dorado-page/circulo-dorado-page.component.ts
@@ -2,21 +2,23 @@ import { Component, EventEmitter, Input } from '@angular/core';
 import { FormularioPreguntasComponent } from '../formulario-preguntas/formulario-preguntas.component';
 import { GraficaCirculoComponent } from '../grafica-circulo/grafica-circulo.component';
 import { HeaderComponent } from '../../header/header.component';
+import { PreguntasService } from '../services/preguntas.service';
 
 @Component({
   selector: 'app-circulo-dorado-page',
   standalone: true,
-  imports: [FormularioPreguntasComponent, GraficaCirculoComponent, HeaderComponent],
+  imports: [
+    FormularioPreguntasComponent,
+    GraficaCirculoComponent,
+    HeaderComponent,
+  ],
   templateUrl: './circulo-dorado-page.component.html',
-  styleUrl: './circulo-dorado-page.component.css'
+  styleUrl: './circulo-dorado-page.component.css',
 })
 export class CirculoDoradoPageComponent {
-  indexPregunta: number = 0;
+  constructor(public preguntaService: PreguntasService) {}
 
   actualizarProgreso(nuevoProgreso: number) {
-    this.indexPregunta = nuevoProgreso;
+    this.preguntaService.indexPregunta = nuevoProgreso;
   }
-
-  // @Input() reinicioFormulario: EventEmitter<void> = new EventEmitter<void>();
-
 }
--- reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/grafica-circulo/grafica-circulo.component.ts
+++ reto1-estramypyme/angularReto1/src/app/domain/circuloDorado/grafica-circulo/grafica-circulo.component.ts
@@ -8,6 +8,7 @@ import {
   OnInit,
 } from '@angular/core';
 import * as echarts from 'echarts';
+import { PreguntasService } from '../services/preguntas.service';
 
 @Component({
   selector: 'app-grafica-circulo',
@@ -18,7 +19,6 @@ import * as echarts from 'echarts';
 })
 export class GraficaCirculoComponent implements AfterViewInit {
   //Recibe el índice de la pregunta actual desde el componente padre.
-  @Input() indexPregunta!: number;
   progress = 0;
   contenedorGrafica: any;
   porQue: number = 0;
@@ -27,7 +27,7 @@ export class GraficaCirculoComponent implements AfterViewInit {
 
   @ViewChild('graficaCirculo') contenedor!: ElementRef;
 
-  constructor() {}
+  constructor(public preguntaService: PreguntasService) {}
 
   //Actualiza el progreso cuando cambia el índice de la pregunta actual
   ngOnChanges(changes: SimpleChanges) {
@@ -39,27 +39,48 @@ export class GraficaCirculoComponent implements AfterViewInit {
   y establece las opciones de la gráfica y actualiza el progreso con el índice de la pregunta actual. */
   ngAfterViewInit() {
     this.contenedorGrafica = echarts.init(this.contenedor.nativeElement);
-    this.actualizarProgreso(this.indexPregunta);
+    this.actualizarProgreso(this.preguntaService.indexPregunta);
+
+    const progresoAlmacenado = localStorage.getItem('progresoGrafica');
+    if (progresoAlmacenado) {
+      const progreso = JSON.parse(progresoAlmacenado);
+      this.porQue = progreso.porQue;
+      this.como = progreso.como;
+      this.que = progreso.que;
+      this.actualizarProgreso(this.preguntaService.indexPregunta);
+    }
   }
 
   /*Actualiza el progreso en la gráfica circular según el índice de la pregunta actual,
   modificando los valores de las series y las opciones de la gráfica.*/
   actualizarProgreso(index: number) {
+    localStorage.setItem(
+      'progresoGrafica',
+      JSON.stringify({
+        porQue: this.porQue,
+        como: this.como,
+        que: this.que,
+      })
+    );
+
     //Lógica para actualizar el progreso en la gráfica
-    if (this.indexPregunta == 0) {
+    if (this.preguntaService.indexPregunta == 0) {
       this.porQue = 5;
       this.como = 5;
       this.que = 5;
-    } else if (this.indexPregunta <= 5) {
-      this.porQue = this.indexPregunta;
+    } else if (this.preguntaService.indexPregunta <= 5) {
+      this.porQue = this.preguntaService.indexPregunta;
       this.como = 0;
       this.que = 0;
-    } else if (this.indexPregunta > 5 && this.indexPregunta <= 10) {
-      this.como = this.indexPregunta - 5;
+    } else if (
+      this.preguntaService.indexPregunta > 5 &&
+      this.preguntaService.indexPregunta <= 10
+    ) {
+      this.como = this.preguntaService.indexPregunta - 4;
       this.porQue = 5;
       this.que = 0;
-    } else if (this.indexPregunta <= 15) {
-      this.que = this.indexPregunta - 10;
+    } else if (this.preguntaService.indexPregunta <= 15) {
+      this.que = this.preguntaService.indexPregunta - 9;
       this.porQue = 5;
       this.como = 5;
     }
