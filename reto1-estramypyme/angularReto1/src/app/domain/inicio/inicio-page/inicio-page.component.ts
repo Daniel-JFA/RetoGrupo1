a34970a1c52d647fe861c19fcd8c1f21287e1da3
Juan Esteban Munoz <juan_987-emr@hotmail.com>
2024-07-29 14:28:02 -0500

    se cambio los botones por la etiqueta a para llevar a la páginas del circulo y el radar

--- reto1-estramypyme/angularReto1/src/app/domain/inicio/inicio-page/inicio-page.component.ts
+++ reto1-estramypyme/angularReto1/src/app/domain/inicio/inicio-page/inicio-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 
 
+
+
 @Component({
   selector: 'app-inicio-page',
   standalone: true,
